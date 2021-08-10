d00a3f1de1b384d8c11c1eea349160468d309508
19012627-Ang-Zhen-Kai <67895467+19012627-Ang-Zhen-Kai@users.noreply.github.com>
2021-08-10 10:16:09 +0800

    Added Appointment methods

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
-
+	
+	static ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public static void viewAppointment(ArrayList<Appointment> appointmentList) {
 
 	}
+	
+	public static void addAppointment(ArrayList<Appointment> appointmentList) {
 
+	}
+	
+	public static void updateAppointment(ArrayList<Appointment> appointmentList) {
+
+	}
+	
+	public static void deleteAppointment(ArrayList<Appointment> appointmentList) {
+
+	}
 }
